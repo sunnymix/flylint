759aa05bc23f2a9798456fcc5565ccd8861156ea
sunnymix <sunny@sunnymix.com>
2022-12-06 09:50:48 +0800

    ui: wiki detail add custom scroll bar

--- ui/src/components/wiki/WikiToc.tsx
+++ ui/src/components/wiki/WikiToc.tsx
@@ -1,6 +1,7 @@
 import { Toc } from './WikiModel';
 import { Button } from 'antd';
 import { useCallback } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars'
 
 export interface WikiTocProps {
   className?: string,
@@ -20,25 +21,25 @@ const WikiToc = (props: WikiTocProps) => {
   
   return (
     <div className={props.className || 'wiki-toc'} style={{ width: props.width || 400}}>
-      <div className='toc-content'>
-        {props.tocData && props.tocData.map((toc: Toc) => (                                                                                                                                                                                                                                                                                                                                                                                                                                 
-          <div key={toc.index}>
-            <a className='toc-item' onClick={(event: any) => onClick(event, toc)}>
-              <div className='toc-lines'>
-                {[...Array(toc.level).keys()].map((ele: any, index: number) => (
-                  <div key={index} className='toc-line'  style={{opacity: 1.0 / (index + 1) * 1.5}}></div>
-                ))}
-              </div>
-              <div className='toc-text'>
-                <div className='toc-indicator'>
-                  <div className='toc-dot'></div>
+        <Scrollbars className='toc-content' autoHide>
+          {props.tocData && props.tocData.map((toc: Toc) => (                                                                                                                                                                                                                                                                                                                                                                                                                                 
+            <div key={toc.index}>
+              <a className='toc-item' onClick={(event: any) => onClick(event, toc)}>
+                <div className='toc-lines'>
+                  {[...Array(toc.level).keys()].map((ele: any, index: number) => (
+                    <div key={index} className='toc-line'  style={{opacity: 1.0 / (index + 1) * 1.5}}></div>
+                  ))}
                 </div>
-                {toc.text}
-              </div>
-            </a>
-          </div>
-        ))}
-      </div>
+                <div className='toc-text'>
+                  <div className='toc-indicator'>
+                    <div className='toc-dot'></div>
+                  </div>
+                  {toc.text}
+                </div>
+              </a>
+            </div>
+          ))}
+        </Scrollbars>     
     </div>
   )
 };
